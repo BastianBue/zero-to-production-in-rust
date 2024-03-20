8d626e9a1f6f92f4d2f60f5067dcc43c8c79149d
Bastian Büld <bastian.bueld@aoe.com>
2024-03-20 21:26:23 +0100

    feat(main): split into lib and main to enable integration testing

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use actix_web::{ web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+async fn index(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    HttpResponse::Ok().body(format!("Hello {}", name))
+}
+
+async fn health_check() -> impl Responder{
+    return HttpResponse::Ok()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(index))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
