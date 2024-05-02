38e220183aac8b2e4929b6af7cc8da3b7e98a88a
Bastian Büld <bastian.bueld@aoe.com>
2024-05-02 20:15:09 +0200

    test: create first tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,21 @@
-use actix_web::{ App, HttpResponse, HttpServer, Responder, get};
+use actix_web::web::Form;
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
+}
+
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+#[post("/signup")]
+async fn signup(form: Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello, {}, {}!", form.name, form.email))
 }
 
 #[get("/health_check")]
@@ -11,7 +24,8 @@ async fn health_check() -> impl Responder {
 }
 
 pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| { App::new().service(index).service(health_check) })
+    HttpServer::new(|| App::new().service(index).service(health_check))
         .bind(("127.0.0.1", 8080))?
-        .run().await
+        .run()
+        .await
 }
--- tests/healt_check.rs
+++ tests/healt_check.rs
@@ -1,5 +1,6 @@
 use actix_web::{http::header::ContentType, test, App};
-use zero_to_production_in_rust::{index};
+use serde_urlencoded;
+use zero_to_production_in_rust::{index, signup};
 
 #[actix_web::test]
 async fn health_check() {
@@ -10,4 +11,54 @@ async fn health_check() {
 
     let resp = test::call_service(&app, req).await;
     assert!(resp.status().is_success());
+}
+
+#[actix_web::test]
+async fn signup_returns_400_with_invalid_data() {
+    let app = test::init_service(App::new().service(signup)).await;
+    let test_cases = vec![
+        ([("name", "Bastian"), ("email", "")], "email is required"),
+        ([("name", ""), ("email", "hi@gmx.de")], "name is required"),
+        ([("name", ""), ("email", "")], "invalid input"),
+    ];
+
+    for (payload, _error_message) in test_cases {
+        let body = serde_urlencoded::to_string(payload).unwrap();
+        let req = test::TestRequest::post()
+            .uri("/signup")
+            .insert_header(ContentType::form_url_encoded())
+            .set_payload(body)
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(
+            resp.status().as_u16(),
+            400,
+            "The request payload: {:?} should have failed with a 400 Bad Request",
+            payload
+        );
+    }
+}
+
+#[actix_web::test]
+async fn signup_returns_200_with_valid_data() {
+    let app = test::init_service(App::new().service(signup)).await;
+    let test_cases = vec![[("name", "Bastian"), ("email", "hi@gmx.de")]];
+
+    for payload in test_cases {
+        let body = serde_urlencoded::to_string(payload).unwrap();
+        let req = test::TestRequest::post()
+            .uri("/signup")
+            .insert_header(ContentType::form_url_encoded())
+            .set_payload(body)
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(
+            resp.status().as_u16(),
+            200,
+            "The request payload: {:?} should have succeeded with a 200 OK",
+            payload
+        );
+    }
 }
